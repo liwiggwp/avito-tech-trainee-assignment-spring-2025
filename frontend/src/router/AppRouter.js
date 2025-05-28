6e272eda53074d7297afdd0f30b91ec3a3258707
Venera Kodo <liwiggwp@gmail.com>
2025-05-28 22:04:39 +0500

    update router name

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import * as Router from "./RouteNames";
 import Home from "../pages/Home";
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={Router.HOME} element={<Home />} />
     </Routes>
   );
 }
--- frontend/src/router/RouteNames.js
+++ frontend/src/router/RouteNames.js
@@ -1,1 +1,4 @@
+export const ID = '/:id';
 export const HOME = '/';
+export const BOARDS = '/boards';
+export const ISSUES = '/issues';
