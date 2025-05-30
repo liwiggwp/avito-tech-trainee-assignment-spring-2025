32cdfd3e5b4020e2262a31f74c4a73aa7d38e80f
Venera Kodo <liwiggwp@gmail.com>
2025-05-30 21:22:08 +0500

    update task statuses and column names

--- frontend/src/utils/Constants.js
+++ frontend/src/utils/Constants.js
@@ -1,8 +1,7 @@
 export const statuses = [
   { id: 1, name: "Backlog" },
-  { id: 2, name: "ToDo" },
-  { id: 3, name: "InProgress" },
-  { id: 4, name: "Done" },
+  { id: 2, name: "InProgress" },
+  { id: 3, name: "Done" },
 ];
 
 export const priorities = [
@@ -12,10 +11,9 @@ export const priorities = [
 ];
 
 export const nameColumn = [
-  { id: 1, name: "Backlog" },
-  { id: 2, name: "To do" },
-  { id: 3, name: "In progress" },
-  { id: 4, name: "Done" },
+  { id: 1, name: "To do" },
+  { id: 2, name: "In progress" },
+  { id: 3, name: "Done" },
 ];
 
 export function getColumnNameById(id) {
